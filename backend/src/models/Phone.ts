5865c92c1bd01931be5a4e45cb8ed6fe7db9396a
vincent <vincent.timo.chi@gmail.com>
2025-12-15 00:16:52 -0800

    refactor: Refactored Simon's Phone.ts and added some extra fields to adhere to frontend design

---
+++ backend/src/models/Phone.ts
@@ -0,0 +1,130 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+// Interfaces (for Review and Phone)
+export interface IReview {
+  id: number;
+  userName: string;
+  rating: number; // 1-5
+  date: string;
+  title: string;
+  review: string;
+  helpful: number;
+  notHelpful: number;
+}
+
+export interface IPhone extends Document {
+  id: string; // The ID like galaxy-s24-ultra -- we're going to use this for url
+  name: string;
+  brand: string;
+  releaseDate: string;
+  price: number;
+  images: {
+    main: string;
+    // Add 'front', 'back', or 'side' here later if we got those pics
+  };
+  specs: {
+    display: {
+      sizeInches: number;
+      resolution: string;
+      refreshRateHz: number;
+      peakBrightnessNits: number;
+      technology: string;
+    };
+    performance: {
+      processor: string;
+      ram: {
+        options: number[];
+        technology: string;
+      };
+      storageOptions: number[]; // [256GB, 512GB, 1TB = 1024GB] -- let's use numbers probably easier to sort/compare
+      cpu: string;
+      gpu: string;
+    };
+    camera: {
+      mainMegapixels: number;
+      ultrawideMegapixels?: number;
+      telephotoMegapixels?: number;
+      frontMegapixels: number;
+      features: string[]; // ["Night Mode", "8K Video"]
+    };
+    battery: {
+      capacitymAh: number;
+      chargingSpeedW: number;
+      wirelessCharging: boolean;
+    };
+    connectivity: {
+      has5G: boolean;
+      hasNfc: boolean;
+      headphoneJack: boolean;
+    };
+  };
+  reviews: IReview[];
+}
+
+// Phone Schema
+const PhoneSchema: Schema = new Schema(
+  {
+    id: { type: String, required: true, unique: true, index: true },
+    name: { type: String, required: true },
+    brand: { type: String, required: true },
+    releaseDate: { type: String },
+    price: { type: Number, required: true },
+    images: {
+      main: { type: String, required: true },
+      // If we have 'front', 'back', 'side' images we can add those fields here with the image/path
+    },
+    specs: {
+      display: {
+        sizeInches: { type: Number, required: true },
+        resolution: { type: String, required: true },
+        refreshRateHz: { type: Number, default: 60 },
+        peakBrightnessNits: { type: Number },
+        technology: { type: String },
+      },
+      performance: {
+        processor: { type: String, required: true },
+        ram: {
+          options: { type: String },
+          technology: { type: String },
+        },
+        storageOptions: [{ type: Number }],
+        cpu: { type: String },
+        gpu: { type: String },
+      },
+      camera: {
+        mainMegapixels: { type: Number, required: true },
+        ultrawideMegapixels: { type: Number },
+        telephotoMegapixels: { type: Number },
+        frontMegapixels: { type: Number },
+        features: [{ type: String }],
+      },
+      battery: {
+        capacitymAh: { type: Number, required: true },
+        chargingSpeedW: { type: Number },
+        wirelessCharging: { type: Boolean, default: false },
+      },
+      connectivity: {
+        has5G: { type: Boolean, default: true },
+        hasNfc: { type: Boolean, default: true },
+        headphoneJack: { type: Boolean, default: false },
+      },
+    },
+    reviews: [
+      {
+        id: Number,
+        userName: String,
+        rating: Number,
+        date: String,
+        title: String,
+        review: String,
+        helpful: { type: Number, default: 0 },
+        notHelpful: { type: Number, default: 0 },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<IPhone>("Phone", PhoneSchema);
