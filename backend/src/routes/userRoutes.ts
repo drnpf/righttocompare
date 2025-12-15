697222343fa15086d3e10255127a69212176492a
vincent <vincent.timo.chi@gmail.com>
2025-12-14 17:08:57 -0800

    refactor: Refactored Dean's routes into TS from profileRoutes.js

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { syncUser } from "../controllers/userController";
+import { syncUser, updateUser } from "../controllers/userController";
 import { protect } from "../middleware/authMiddleware";
 
 const router: Router = express.Router();
@@ -9,6 +9,12 @@ const router: Router = express.Router();
  * @desc    Sync user data from Firebase to MongoDB
  */
 router.post("/sync", protect, syncUser);
+
+/**
+ * @route  PUT /api/users/:uid
+ * @desc   Update user profile data in MongoDB
+ */
+router.put("/:uid", protect, updateUser);
 
 // Add more routes here related to USER
 
