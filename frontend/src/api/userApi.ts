8668c17987f81d78554b3d3b92ca35e6b90240af
vincent <vincent.timo.chi@gmail.com>
2025-12-14 19:07:13 -0800

    feat: Implemented getUserProfile bridging function

--- frontend/src/api/userApi.ts
+++ frontend/src/api/userApi.ts
@@ -8,6 +8,7 @@ const API_URL = "http://localhost:5000/api/users"; // CHANGE LATER ON PRODUCTION
  * Backend verifies the token and upon successful verification, user
  * data retrieves or creeates the user's profile in MongoDB.
  * @param firebaseUser The user object from Firebase Auth
+ * @returns The AppUser object, or null if failed
  */
 export const syncUserWithBackend = async (firebaseUser: User | null) => {
   if (!firebaseUser) return null;
@@ -39,10 +40,44 @@ export const syncUserWithBackend = async (firebaseUser: User | null) => {
 };
 
 /**
+ * Retrieves the user's profile data from the backend.
+ * Should be called on page refresh to restore user session or when
+ * no need to update/write into the database.
+ * @param uid The Firebase UID of the user
+ * @param token The Firebase Auth ID token (for security)
+ * @returns The AppUser object, or null if failed
+ */
+export const getUserProfile = async (uid: string, token: string) => {
+  try {
+    const response = await fetch(`${API_URL}/${uid}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    // Handles failed sync with backend
+    if (!response.ok) {
+      throw new Error("Failed to sync user with backend");
+    }
+
+    // Returns user data
+    const userData = await response.json();
+    console.log("Backend Retrieval Success:", userData);
+    return userData;
+  } catch (error) {
+    console.error("Error getting profile:", error);
+    return null;
+  }
+};
+
+/**
  * Updates the user's profile in the backend.
  * @param uid The Firebase UID of the user
  * @param token The Firebase Auth ID token (for security)
  * @param updates The partial data to update (displayName, preferences, wishlist).
+ * @returns The updated AppUser object, or null if failed
  */
 export const updateUserProfile = async (uid: string, token: string, updates: Partial<AppUser>) => {
   try {
