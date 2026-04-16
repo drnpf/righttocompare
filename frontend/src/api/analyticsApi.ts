67b2537ab46079e320ca23fc8b2167c8ab1202ed
vincent <vincent.t.chi@gmail.com>
2026-04-16 15:13:59 -0700

    feature: Added frontend bridging function to get a list of popular comparisons within a specified time period and a specified number of comparisons to pull. Added a data mapper for the popular comparison object type to map JSON to the popular comparison object.

--- frontend/src/api/analyticsApi.ts
+++ frontend/src/api/analyticsApi.ts
@@ -1,3 +1,6 @@
+import { PopularComparison } from "../types/analyticsTypes";
+import { mapJsonToPopularComparison } from "../utils/analyticsDataMappers";
+
 const API_URL = "http://localhost:5001/api/analytics"; // CHANGE LATER ON PRODUCTION
 
 /**
@@ -14,10 +17,31 @@ export const logComparison = async (phoneIds: string[]): Promise<void> => {
     });
 
     // Logging that analytics failed on backend
-    if (!response.ok) {
-      console.warn("Analytics log failed.");
-    }
+    if (!response.ok) console.warn("Analytics log failed.");
   } catch (error) {
     console.error("Analytics error:", error);
+  }
+};
+
+/**
+ * Fetches a list of the most popular comparisons.
+ * @param days The number of days to include comparisons from
+ * @param limit Maximum number of comparison results to pull
+ * @returns A list of PopularComparison objects
+ */
+export const getPopularComparisons = async (days: number, limit: number): Promise<PopularComparison[]> => {
+  try {
+    const url = `${API_URL}/trending?days=${days}&limit=${limit}`;
+    const response = await fetch(url);
+
+    // Handles failed connection to backend
+    if (!response.ok) throw new Error(`Popular comparions fetch failed: ${response.status}`);
+
+    // Mapping each PhoneSummary objects to their respective comparisons
+    const rawData = await response.json();
+    return rawData.map((item: any) => mapJsonToPopularComparison(item));
+  } catch (error) {
+    console.error("Error fetching popular comparisons:", error);
+    return [];
   }
 };
---
+++ frontend/src/utils/analyticsDataMappers.ts
@@ -0,0 +1,14 @@
+import { PopularComparison } from "../types/analyticsTypes";
+import { mapJsonToPhoneSummary } from "./phoneDataMappers";
+
+/**
+ * Maps a popular comparison raw JSON to contain phone summaries of phones
+ * in the comparisons.
+ * @param dbComparison The raw comparison JSON object from the analytics API
+ * @returns A PopularComparison object containing an array of phone summaries.
+ */
+export const mapJsonToPopularComparison = (dbComparison: any): PopularComparison => {
+  return {
+    phones: (dbComparison.phones || []).map((phone: any) => mapJsonToPhoneSummary(phone)),
+  };
+};
